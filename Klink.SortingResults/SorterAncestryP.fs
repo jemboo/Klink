2a0e05f11cd823053c6c0980113eafff9f58df53
Tom Patterson <jemboo@gmail.com>
2023-09-27 11:20:50 -0400

    Update SorterAncestryP.fs

--- Klink.SortingResults/SorterAncestryP.fs
+++ Klink.SortingResults/SorterAncestryP.fs
@@ -125,55 +125,54 @@ module SorterSetAncestryP =
         }
 
 
-    //let update 
-    //        (newId:sorterSetAncestryId)
-    //        (sorterSetEval:sorterSetEval)
-    //        (stageWeight:stageWeight)
-    //        (parentMap:Map<sorterId, sorterParentId>)
-    //        (sorterSetAncestry:sorterSetAncestryP) 
-    //    =
-    //    let nextGen = (sorterSetAncestry.generation |> Generation.next)
+    let update 
+            (newId:sorterSetAncestryId)
+            (sorterSetEval:sorterSetEval)
+            (stageWeight:stageWeight)
+            (parentMap:Map<sorterId, sorterParentId>)
+            (sorterSetAncestry:sorterSetAncestryP) 
+        =
+        let nextGen = (sorterSetAncestry.generation |> Generation.next)
 
-    //    let _updateSorterAncestry 
-    //            (parentSorterAncestry:sorterAncestryP)
-    //            (sev:sorterEval)
-    //            (gen:generation)
-    //            (sw:stageWeight)
-    //        =
-    //        let sorterFitness = (sev.sorterSpeed |> Option.get |> SorterFitness.fromSpeed sw)
+        let _updateSorterAncestry 
+                (parentSorterAncestry:sorterAncestryP)
+                (sev:sorterEval)
+                (gen:generation)
+                (stageWeight:stageWeight)
+            =
+            SorterAncestryP.update
+                parentSorterAncestry
+                sev.sortrId
+                gen
+                (sev.sortrPhenotypeId |> Option.get)
+                (sev.sorterSpeed |> Option.get |> SorterFitness.fromSpeed stageWeight)
 
-    //        SorterAncestryP.update
-    //            parentSorterAncestry
-    //            sev.sortrId
-    //            gen
-    //            (sev.sortrPhenotypeId |> Option.get)
-    //            (sev.sorterSpeed |> Option.get |> SorterFitness.fromSpeed sw)
+        let evalMap = sorterSetEval |> SorterSetEval.getSorterEvalsMap
 
-    //    let _update 
-    //            (sorterId, sorterParentId) =
-    //        if (sorterId |> SorterId.value) = (sorterParentId |> SorterParentId.value) 
-    //           then
-    //            (sorterId, sorterSetAncestry.ancestorMap.[sorterId])
-    //           else
-    //            let parentSorterAncestry = 
-    //                sorterSetAncestry.ancestorMap.[sorterParentId |> SorterParentId.toSorterId]
-    //            (
-    //                sorterId,
-    //                _updateSorterAncestry
-    //                    parentSorterAncestry 
-    //                    sorterId
-    //                    nextGen
-    //              )
+        let _update  (sorterId, sorterParentId) =
+            if (sorterId |> SorterId.value) = (sorterParentId |> SorterParentId.value) 
+               then
+                (sorterId, sorterSetAncestry.ancestorMap.[sorterId])
+               else
+                let parentSorterAncestry = 
+                    sorterSetAncestry.ancestorMap.[sorterParentId |> SorterParentId.toSorterId]
+                (
+                    sorterId,
+                    _updateSorterAncestry
+                        parentSorterAncestry 
+                        evalMap.[sorterId]
+                        nextGen
+                        stageWeight
+                  )
 
-    //    let newAncestorMap = 
-    //        parentMap
-    //        |> Map.toSeq 
-    //        |> Seq.map(_update)
-    //        |> Map.ofSeq
+        let newAncestorMap = 
+            parentMap
+            |> Map.toSeq 
+            |> Seq.map(_update)
+            |> Map.ofSeq
 
-    //    {
-    //        id = newId;
-    //        generation = nextGen; 
-    //        ancestorMap = newAncestorMap
-    //    }
-
+        {
+            id = newId;
+            generation = nextGen; 
+            ancestorMap = newAncestorMap
+        }
